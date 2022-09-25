858835767b22f1c5cd10cf3baa34bd7b7a77c978
BlueBerriee <60186647+BlueBerriee@users.noreply.github.com>
2022-09-25 19:56:23 -0400

    Add files via upload

---
+++ frames.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import cv2
+vidcap = cv2.VideoCapture('bad apple.mp4')
+success,image = vidcap.read()
+count = 0
+while success:
+    cv2.imwrite("C:/Users/ayaeg/Documents/music_ascii/frames/frame%d.jpg" % count, image)     # save frame as JPEG file
+    success,image = vidcap.read()
+    print('Read a new frame: ', success)
+    count += 1
