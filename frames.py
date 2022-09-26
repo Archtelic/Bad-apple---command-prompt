b7f85687c4d8db3400d68176524062b10b411000
BlueBerriee <60186647+BlueBerriee@users.noreply.github.com>
2022-09-26 12:38:59 -0400

    Update frames.py

--- frames.py
+++ frames.py
@@ -4,7 +4,7 @@ vidcap = cv2.VideoCapture('bad apple.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
-    cv2.imwrite("/frames/frame%d.jpg" % count, image)     # save frame as JPEG file
+    cv2.imwrite("frames/frame%d.jpg" % count, image)     # save frame as JPEG file
     success,image = vidcap.read()
     print('Read a new frame: ', success)
     count += 1
